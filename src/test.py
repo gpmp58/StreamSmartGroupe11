f2bffbc4d8e2d12575b20ff233c6b838cde2ff3c
malolelourd <malo.le-lourd@eleve.ensai.fr>
2024-09-06 15:07:38 +0200

    modif test

--- src/test.py
+++ src/test.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import json
+
 load_dotenv()
 
 cle_api = os.environ.get("API_KEY")
@@ -9,15 +10,12 @@ cle_api = os.environ.get("API_KEY")
 movie_name = "batman"
 url = f"https://api.themoviedb.org/3/search/movie?query={movie_name}&include_adult=false&language=en-US&page=1"
 
-headers = {
-    "accept": "application/json",
-    "Authorization": f"Bearer {cle_api}"
-}
+headers = {"accept": "application/json", "Authorization": f"Bearer {cle_api}"}
 
 response = requests.get(url, headers=headers)
 
 print(response.status_code)
-#print(response.text)
+# print(response.text)
 data = json.loads(response.content)
 
 for i in data["results"]:
