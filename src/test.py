7816d9940e7e2e863252627344bb559f9226dc8c
ikram_pro <ikramdrine22@gmail.com>
2024-09-13 10:50:19 +0200

    projet

--- src/test.py
+++ src/test.py
@@ -17,8 +17,43 @@ headers = {
 response = requests.get(url, headers=headers)
 
 print(response.status_code)
-# print(response.text)
+print(response.text)
 data = json.loads(response.content)
 
-for i in data["results"]:
-    print(i["original_title"])
+#for i in data["results"]:
+    #print(i["original_title"])
+
+########################## test providers film ############################
+def get_providers_for_movie(movie_name: str):
+    url_search_movie = f"https://api.themoviedb.org/3/search/movie?query={movie_name}&include_adult=false&language=en-US&page=1"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {cle_api}"
+    }
+    response = requests.get(url_search_movie, headers=headers)
+
+    data = json.loads(response.content)
+    premier_resultat = data["results"][0]
+    print("film: " + premier_resultat["original_title"])
+    movie_id = premier_resultat["id"]
+
+    url_movie_providers = f"https://api.themoviedb.org/3/movie/{movie_id}/watch/providers"
+
+    response = requests.get(url_movie_providers, headers=headers)
+    print(response.status_code)
+    print(response.text)
+
+    data = json.loads(response.content)
+
+    result_fr = data["results"]["FR"]
+
+    if "flatrate" in result_fr.keys():
+        print("--- Plateforme ---")
+        result_flatrate = result_fr["flatrate"]
+        for provider in result_flatrate:
+            print(provider["provider_name"])
+    else:
+        print("pas dispo en streaming")
+
+
+get_providers_for_movie("Batman")
