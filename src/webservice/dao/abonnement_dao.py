fce4a61ec5ca88157bac1498a9d37188df824a2f
guedemi <ali-nour-guedemi.abdelwahid@eleve.ensai.fr>
2024-10-22 15:06:04 +0200

    modif d abonnement dao

--- src/webservice/dao/abonnement_dao.py
+++ src/webservice/dao/abonnement_dao.py
@@ -3,13 +3,16 @@ from src.webservice.dao.db_connection import DBConnection
 
 from src.webservice.business_object.abonnement import Abonnement
 
+from src.webservice.business_object.plateforme import Plateforme
+
 
 class AbonnementDao():
     """Classe contenant les méthodes pour recuperer les infos d'un abonnement"""
 
-    def rechercher_prix_DAO(self,  abonnement: Abonnement) -> bool:
+    def rechercher_prix_DAO(self,  abonnement: Abonnement) -> tuple:
         """Cette méthode recherche les infos sur un abonnement"""
 
+        prix = None
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -17,18 +20,15 @@ class AbonnementDao():
                     " WHERE id_abonnement = %(id_abonnement)s      ",
                     {"id_abonnement": abonnement.id_abonnement},
                 )
-                res = cursor.fetchone()
-
-        prix = None
-        if res:
-            prix = res
+                prix = cursor.fetchone()
 
         return prix
 
 
-    def rechercher_pub_DAO(self,  abonnement: Abonnement) -> bool:
+    def rechercher_pub_DAO(self,  abonnement: Abonnement) -> tuple:
         """Cette méthode recherche les infos sur un abonnement"""
         
+        pub = None
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -36,17 +36,15 @@ class AbonnementDao():
                     " WHERE id_abonnement = %(id_abonnement)s      ",
                     {"id_abonnement": abonnement.id_abonnement},
                 )
-                res = cursor.fetchone()
-
-        pub = None
-        if res:
-            pub = res
+                pub = cursor.fetchone()
 
         return pub
     
-def rechercher_qualite_DAO(self,  abonnement: Abonnement) -> bool:
+    
+def rechercher_qualite_DAO(self,  abonnement: Abonnement) -> tuple:
         """Cette méthode recherche les infos sur un abonnement"""
         
+        qualite = None
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -54,10 +52,22 @@ def rechercher_qualite_DAO(self,  abonnement: Abonnement) -> bool:
                     " WHERE id_abonnement = %(id_abonnement)s      ",
                     {"id_abonnement": abonnement.id_abonnement},
                 )
-                res = cursor.fetchone()
-
-        qualite = None
-        if res:
-            qualite = res
+                qualite = cursor.fetchone()
 
         return qualite
+    
+def rechercher_abonnement(self, plateforme: Plateforme) -> list:
+        """
+        Cette méthode recherche les abonnements d'une plateforme"""
+        abonnements = []
+        with DBConnection().connection as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT id_abonnement   "
+                    "FROM utilisateur           "
+                    "WHERE nom_plateforme = %(nom_plateforme)s",
+                    {"nom_plateforme": abonnement.nom_plateforme},
+                )
+                abonnements = fetchall()
+
+        return abonnements
