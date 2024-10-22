353e801da8a36c64724503284da3801684c08da4
ikram_pro <ikramdrine22@gmail.com>
2024-10-22 11:17:18 +0200

    test

--- src/webservice/services/service_plateforme.py
+++ src/webservice/services/service_plateforme.py
@@ -1,4 +1,5 @@
 from src.webservice.business_object.plateforme import PlateformeStreaming
+from src.webservice.dao.plateforme_dao import PlateformeDAO
 
 class service_plateforme():
     def mettre_a_jour_plateforme(self, id_plateforme, nom_plateforme):
