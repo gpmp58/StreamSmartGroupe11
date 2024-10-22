bee05fa42a37169fb6aea041004f38cee0f32ea2
ikram_pro <ikramdrine22@gmail.com>
2024-10-22 15:06:40 +0200

    abonnementservice

---
+++ src/webservice/services/service_abonnement.py
@@ -0,0 +1,28 @@
+from src.webservice.business_object.plateforme import PlateformeStreaming
+from src.dao.abonnement_dao import AbonnementDao
+class AbonnementService():
+
+    def prix_abonnement(self, id_abonnement):
+        abonnement = Abonnement(id_abonnement)
+        prix = AbonnementDao().rechercher_prix_DAO(abonnement)
+        return prix
+
+    def pub_abonnement(self, id_abonnement):
+        abonnement = Abonnement(id_abonnement)
+        pub = AbonnementDao().rechercher_pub_DAO(abonnement)
+        if pub :
+            return f"Cet abonnement contient des pub !"
+        else :
+            return f"Cet abonnement ne contient pas des pub !"
+
+    def qualite_abonnement(self, id_abonnement):
+        abonnement = Abonnement(id_abonnement)
+        qualite = AbonnementDao().rechercher_qualite_DAO(abonnement)
+        return qualite
+    
+    def recherche_abonnement(self, nom_plateforme, id_plateforme):
+        plateforme = PlateformeStreaming(nom_plateforme, id_plateforme)
+        abonnement_list = AbonnementDao().rechercher_abonnement(plateforme)
+        return abonnement_list
+
+    
